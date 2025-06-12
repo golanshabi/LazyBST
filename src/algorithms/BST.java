70017fc4c7d5138369882d951ede1c3b39e5942f
Golan Shabi <golan.shabi@vastdata.com>
2025-06-12 13:44:25 +0300

    Completed add function by a simple: find leaf, lock it, add next algorithm

--- src/algorithms/BST.java
+++ src/algorithms/BST.java
@@ -4,46 +4,56 @@ class BST {
         volatile Node right;
         volatile Node left;
         volatile boolean marked;
+        public Node(int key) { // Node constructor
+            this.key = key;
+            this.left = null;
+            this.right = null;
+            this.marked = false;
+        }
     }
 
-    final Node root;
-
-    public Node(int key) { // Node constructor
-        this.key = key;
-        this.left = null;
-        this.rigt = null;
-        this.marked = false;
-    }
+    final Node root = new Node(Integer.MIN_VALUE);
 
     boolean contains(int key) {
         // IMPLEMENT
+        return false;
+    }
+
+    boolean validateLeaf(Node curr, int key) {
+        return !curr.marked && (curr.key > key && curr.left == null || curr.key < key && curr.right == null);
     }
 
     boolean validate(Node pred, Node curr) {
-        return !pred.marked && !curr.marked && pred.next == curr;
+        return !pred.marked && !curr.marked && (pred.right == curr || pred.left == curr);
     }    
 
     boolean add(int key) {
         while (true) {
-            Node pred = head;
-            Node curr = pred.next; 
-            while (curr.key < key) {
-                pred = curr; curr = curr.next;
+            Node pred = root;
+            Node curr = root.key < key ? root.right : root.left;
+            boolean foundNode = false;
+            while (curr != null) {
+                if (curr.key > key) {
+                    pred = curr;
+                    curr = curr.left;
+                } else if (curr.key < key) {
+                    pred = curr;
+                    curr = curr.right;
+                } else {
+                    return false;
+                }
             }
             synchronized(pred) {
-                synchronized(curr) {
-                    if (validate (pred, curr)) {
-                        if (curr.key == key) {
-                            return false;
-                        } else {
-                            Node node = new Node(key);
-                            node.next = curr;
-                            pred.next = node;
-                            return true;
-                        } 
-                    } 
-                } 
+                if (validateLeaf(pred, key)) {
+                    Node node = new Node(key);
+                    if (key < pred.key) {
+                        pred.left = node;
+                    } else {
+                        pred.right = node;
+                    }
+                    return true;
+                }
             } 
-        } 
+        }
     }
 }
