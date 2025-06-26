2bc580646939324979fa3ad7c740c545278d4dd2
Ron Geva <coolron54@gmail.com>
2025-06-26 23:57:38 +0300

    added remove functionality to BST tree

--- src/algorithms/BST.java
+++ src/algorithms/BST.java
@@ -89,4 +89,118 @@ public class BST {
             } 
         }
     }
+
+    private boolean removeRight(Node pred, Node curr) {
+        if (curr.right == null) {
+            pred.right = curr.left;
+            return true;
+        }
+
+        Node smallest_in_right = curr.right;
+        Node smallest_pred = curr;
+        while (smallest_in_right.left != null) {
+            smallest_pred = smallest_in_right;
+            smallest_in_right = smallest_in_right.left;
+        }
+
+        Node biggest_in_smallest_subtree = smallest_in_right;
+        while (biggest_in_smallest_subtree.right != null) {
+            biggest_in_smallest_subtree = biggest_in_smallest_subtree.right;
+        }
+
+        synchronized (smallest_in_right) {
+            if (smallest_in_right.left != null || smallest_in_right.marked)
+                return false;
+
+            synchronized (biggest_in_smallest_subtree) {
+                if (biggest_in_smallest_subtree.right != null || biggest_in_smallest_subtree.marked)
+                    return false;
+
+                pred.right = smallest_in_right;
+                smallest_in_right.left = curr.left;
+                biggest_in_smallest_subtree.right = curr.right;
+                smallest_pred.left = null;
+                return true;
+            }
+        }
+    }
+
+    private boolean removeLeft(Node pred, Node curr) {
+        if (curr.left == null)
+        {
+            pred.left = curr.right;
+            return true;
+        }
+
+        Node biggest_in_left = curr.left;
+
+        Node biggest_pred = curr;
+        while (biggest_in_left.right != null) {
+            biggest_pred = biggest_in_left;
+            biggest_in_left = biggest_in_left.right;
+        }
+
+        Node smallest_in_biggest_subtree = biggest_in_left;
+        while (smallest_in_biggest_subtree.left != null) {
+            smallest_in_biggest_subtree = smallest_in_biggest_subtree.left;
+        }
+
+        synchronized (biggest_in_left) {
+            if (biggest_in_left.right != null || biggest_in_left.marked)
+                return false;
+
+            synchronized (smallest_in_biggest_subtree) {
+                if (smallest_in_biggest_subtree.left != null || smallest_in_biggest_subtree.marked)
+                    return false;
+
+                pred.left = biggest_in_left;
+                biggest_in_left.right = curr.right;
+                smallest_in_biggest_subtree.left = curr.left;
+                // this is true for everyone except curr, but if biggest_pred == curr,
+                // then we don't really care about biggest_pred since curr will now be removed
+                biggest_pred.right = null;
+                return true;
+            }
+        }
+    }
+
+    boolean remove(int key) {
+        while (true) {
+            Node pred = root;
+            Node curr = root.key < key ? root.right : root.left;
+            boolean foundNode = false;
+            while (curr != null) {
+                if (curr.key > key) {
+                    pred = curr;
+                    curr = curr.left;
+                } else if (curr.key < key) {
+                    pred = curr;
+                    curr = curr.right;
+                } else {
+                    foundNode = true;
+                    break;
+                }
+            }
+
+            if (!foundNode)
+                return false;
+
+            synchronized (pred) {
+                synchronized (curr) {
+                    if (!validate(pred, curr))
+                        continue;
+
+                    curr.marked = true;
+
+                    if (curr == pred.left) {
+                        if (removeLeft(pred, curr))
+                            return true;
+                    } else {
+                        if (removeRight(pred, curr))
+                            return true;
+                    }
+                }
+            }
+        }
+    }
 }
